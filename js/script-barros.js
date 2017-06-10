d19027bc6a73b45ad67fc81ff51ab0381c27cf77
Matheus Freitas <matheusfreitas70@gmail.com>
2017-06-10 20:49:20 -0300

    Add files via upload

---
+++ js/script-barros.js
@@ -0,0 +1,17 @@
+//Estilo Java Script personalizado por Matheus Freitas
+// para o site Barros Corretora
+
+
+$(function(){
+    //carregamento da página
+    
+    $("#modal01").modal({backdrop:true, keyboard:false});
+    
+    
+    $("#modal01").modal("hide");
+    
+   setTimeout(function(){
+       $("#modal01").modal("hide");
+   }, 188000);
+              
+}); //encerramento da função
